74525c56b7187901340289d8ec965f661ecbc59e
madhuvani-dev <madhuvanisiliveru1017@gmail.com>
2026-07-31 19:06:22 +0530

    Modified home page and css, added basic backend for home page.

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
- 
+from flask import Flask, render_template
+from database.db import get_db_connection
+
+# Create Flask application
+app = Flask(__name__)
+
+# Secret key (required later for sessions)
+app.secret_key = "medconnect_secret_key"
+
+# Home Page
+@app.route("/")
+def home():
+    conn = get_db_connection()
+
+    conn.execute("SELECT 1")
+
+    conn.close()
+
+    return render_template("index.html")
+
+# Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
